6c5836002e12266ae00ffed177aefc6440dfc4be
Abhishek Ray <142203624+mrjackff@users.noreply.github.com>
2024-08-07 13:39:04 +0530

    Update api-route.js

--- routes/api-route.js
+++ routes/api-route.js
@@ -1,26 +1,34 @@
 const express = require('express');
 const router = express.Router();
-router.get('user/list',function(req,res){
-    let userData = [
-        {
-        name : "ABC",
-        email : "abc@mail.com"
-        },
-        {
-            name : "ABC1",
-            email : "abc@mail.com"
-        },
-        {
-            name : "ABC2",
-            email : "abc@mail.com"
-        },
-        {
-            name : "ABC3",
-            email : "abc@mail.com"
-        }
-    ]
-    res.send({
-        data:userData,
-        status : "Success"
-    })
-});
+const apiController = require('../controllers/api-controller');
+
+router.get('/user/list',apiController.getUser);
+
+
+// router.get('/user/list',function(req,res){
+//     let userData = [
+//         {
+//         name : "ABC",
+//         email : "abc@mail.com"
+//         },
+//         {
+//             name : "ABC1",
+//             email : "abc@mail.com"
+//         },
+//         {
+//             name : "ABC2",
+//             email : "abc@mail.com"
+//         },
+//         {
+//             name : "ABC3",
+//             email : "abc@mail.com"
+//         }
+//     ]
+//     res.send({
+//         data:userData,
+//         status : "Success"
+//     })
+// });
+module.exports = {
+    route: router
+}
